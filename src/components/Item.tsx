d1c866426ce281a75d7f5b35e8ca16e368b2e513
onesine <onesine@dmdconsul.com>
2022-11-22 10:57:00 +0100

    Theming feature add  🎉

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -16,11 +16,6 @@ const Item: React.FC<ItemProps> = ({item, primaryColor}) => {
         return value !== null && !Array.isArray(value) && value.value === item.value;
     }, [item.value, value]);
 
-    const textColor = useMemo(() => {
-        // @ts-ignore
-        return COLORS.includes(primaryColor) ? THEME_DATA.text[primaryColor] : THEME_DATA.text[DEFAULT_THEME];
-    }, [primaryColor]);
-
     const textHoverColor = useMemo(() => {
         // @ts-ignore
         return COLORS.includes(primaryColor) ? THEME_DATA.textHover[primaryColor] : THEME_DATA.textHover[DEFAULT_THEME];
@@ -47,7 +42,7 @@ const Item: React.FC<ItemProps> = ({item, primaryColor}) => {
                     aria-selected={isSelected}
                     role={"option"}
                     onClick={() => handleValueChange(item)}
-                    className={`block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${isSelected ? `text-white ${bgColor}` : `${textColor} ${bgHoverColor} ${textHoverColor}` }`}
+                    className={`block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${isSelected ? `text-white ${bgColor}` : `text-gray-500 ${bgHoverColor} ${textHoverColor}` }`}
                 >
                     {item.label}
                 </li>
